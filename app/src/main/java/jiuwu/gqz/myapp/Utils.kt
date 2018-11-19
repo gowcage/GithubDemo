d0e22b24e03b37dc8124c7f0b51171574d6d090f
gowcage <gowcage@163.com>
2018-11-19 14:14:58 +0800

    一些简单的测试方法

---
+++ app/src/main/java/jiuwu/gqz/myapp/Utils.kt
@@ -0,0 +1,11 @@
+package jiuwu.gqz.myapp
+
+class Utils {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    val product: (Int, Int) -> Int = { a, b -> a * b }
+
+    val quotient = { a: Int, b: Int -> a / b }
+}
