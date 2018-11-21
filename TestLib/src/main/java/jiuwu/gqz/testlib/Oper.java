68b2022ad8fd19f2bc89ced25d124f8703f2144e
gowcage <gowcage@163.com>
2018-11-21 11:16:21 +0800

    增加Oper类，包含两个方法，求和、求平均数

---
+++ TestLib/src/main/java/jiuwu/gqz/testlib/Oper.java
@@ -0,0 +1,16 @@
+package jiuwu.gqz.testlib;
+
+public class Oper {
+    public Oper() {
+
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public float ave(int a, int b) {
+        return ((float) a + (float) b) / 2f;
+    }
+
+}
